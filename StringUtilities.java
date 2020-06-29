9569cd029a473888033f311701db41a75280799f
Swapnil Srivastava <srivastavas@g.cofc.edu>
2020-06-29 11:47:36 -0400

    implimented

--- StringUtilities.java
+++ StringUtilities.java
@@ -5,7 +5,7 @@ public class StringUtilities {
      * @return `input`
      */
     public String returnInput(String input) {
-        return null;
+        return input;
     }
 
     /**
@@ -14,7 +14,7 @@ public class StringUtilities {
      * @return the concatenation of `baseValue` and `valueToBeAdded`
      */
     public String concatenate(String baseValue, String valueToBeAdded) {
-        return null;
+        return (baseValue + valueToBeAdded);
     }
 
     /**
@@ -22,7 +22,11 @@ public class StringUtilities {
      * @return identical string with characters in opposite order
      */
     public String reverse(String valueToBeReversed) {
-        return null;
+        String rev = "";
+        for(int i = 0; i<valueToBeReversed.length(); i++){
+            rev = rev + valueToBeReversed.charAt(valueToBeReversed.length()-1-i);
+        }
+        return rev;
     }
 
     /**
